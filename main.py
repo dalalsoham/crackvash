0148c2cac96523712d7fadb30f8e107c60913e55
Itsmmdoha <70005698+Itsmmdoha@users.noreply.github.com>
2023-03-08 14:34:48 +0600

    prettifying input methods

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from assist import ChangePassword, getRegNumber
 
 print("""
-what do want?
+what do you want?
+
 1) Get registration number from Nickname & Phone Number
                       Or,
 2) Change pasword using Phone Number & Registration number
 
-
-
 """)
 
-choise = int(input("(1/2): "))
+choice = int(input("(1/2): "))
 phone = input("PhoneNumber(example:88017********): ")
-if choise==1:
+if choice==1:
     nickname = input("Type nickname(case sensitive): ")
     target = getRegNumber(phone,nickname)
     target.req_otp()
     target.crack_otp()
-elif choise==2:
+elif choice==2:
     registration = input("Registration number:")
     target = ChangePassword(phone, registration)
     target.req_otp()

